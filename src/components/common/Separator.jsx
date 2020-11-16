73f1d1229c85f5f3ab8b873f5f50b68904d852b6
TonyLee <tonylee838@foxmail.com>
2020-11-16 21:07:26 +0800

    Refactor: Improve interface for Separator Component.

--- src/components/common/Separator.jsx
+++ src/components/common/Separator.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-
 import { createUseStyles } from "react-jss";
 
 import colors from "../../config/color";
 
-function Separator({ color }) {
-  const styles = useStyle();
+function Separator({ color, width }) {
+  const styles = useStyle({ color, width });
 
   return <div className={styles.line}></div>;
 }
 
 const useStyle = createUseStyles({
   line: {
-    width: "100%",
+    width: ({ width }) => (width ? width : "100%"),
     height: "2px",
     opacity: "0.8",
-    backgroundColor: (color) => (color ? color : colors.light),
+    backgroundColor: ({ color }) => (color ? color : colors.light),
   },
 });
 
