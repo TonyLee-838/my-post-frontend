34364f918b803833ebd81cfd93571a8ddad9ba84
TonyLee <tonylee838@foxmail.com>
2020-11-21 22:49:28 +0800

    Add type component for editing different contents(eg. post,piece...)..

---
+++ src/components/edit/TypeBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { Link } from "react-router-dom";
+import fontFamilies from "../../config/fontFamily";
+import TypeItem from "./TypeItem";
+
+const types = [
+  {
+    icon: "MdDescription",
+    label: "Post",
+    path: "/edit/post",
+  },
+  {
+    icon: "MdCropLandscape",
+    label: "Piece",
+    path: "/edit/piece",
+  },
+  {
+    icon: "MdImportantDevices",
+    label: "Project",
+    path: "/edit/project",
+  },
+];
+
+const TypeBar = (props) => {
+  const classes = useStyle();
+  return (
+    <div className={classes.container}>
+      {types.map((type) => (
+        <TypeItem icon={type.icon} label={type.label} path={type.path} />
+      ))}
+    </div>
+  );
+};
+
+const useStyle = createUseStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "initial",
+    width: "15%",
+    padding: "15px",
+    margin: "15px",
+  },
+});
+
+export default TypeBar;
---
+++ src/components/edit/TypeItem.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { Link } from "react-router-dom";
+import colors from "../../config/color";
+import fontFamilies from "../../config/fontFamily";
+import IconItem from "../common/IconItem";
+
+const TypeItem = ({ icon, label, path }) => {
+  const classes = useStyle();
+  return (
+    <Link key={`type-${label}`} to={path} className={classes.type}>
+      <IconItem icon={icon} label={label} />
+    </Link>
+  );
+};
+
+const useStyle = createUseStyles({
+  type: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "start",
+
+    width: "100%",
+    padding: "15px",
+    marginBottom: "3px",
+
+    textDecoration: "none",
+    borderRadius: "5px",
+    boxShadow: "3px 3px 3px " + colors.medium,
+    cursor: "pointer",
+
+    "& *": {
+      cursor: "pointer",
+    },
+
+    "& label": {
+      color: colors.black,
+      textDecoration: "none",
+      fontFamily: fontFamilies.round,
+      cursor: "pointer",
+      padding: "0px 20px 0px 20px",
+    },
+  },
+});
+
+export default TypeItem;
